6089b915b75de4a51d105c114c4ea8ece8db01bc
agile-seo-dev <agile-seo-dev@users.noreply.github.com>
2024-03-12 21:44:16 +0000

    Added further projects as test

--- modules/Volo.BasicTheme/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic/BasicTheme.cs
+++ modules/Volo.BasicTheme/src/Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic/BasicTheme.cs
@@ -19,18 +19,30 @@ public class BasicTheme : ITheme, ITransientDependency
                 return $"~/Themes/Basic/Layouts/Account.cshtml";
             case BasicLayouts.Empty:
                 return $"~/Themes/Basic/Layouts/Empty.cshtml";
-            case ErindOnTrackLayouts.Application:
+            case EoTLayouts.Application:
                 return $"~/Themes/Basic/EoT/Layouts/Application.cshtml";
-            case ErindOnTrackLayouts.Account:
+            case EoTLayouts.Account:
                 return $"~/Themes/Basic/EoT/Layouts/Account.cshtml";
-            case ErindOnTrackLayouts.Empty:
+            case EoTLayouts.Empty:
                 return $"~/Themes/Basic/EoT/Layouts/Empty.cshtml";
             case MdwLayouts.Application:
-                return $"~/Themes/Basic/Layouts/Mdw/Application_.cshtml";
+                return $"~/Themes/Basic/Layouts/Mdw/Application.cshtml";
             case MdwLayouts.Account:
                 return $"~/Themes/Basic/Layouts/Mdw/Account.cshtml";
             case MdwLayouts.Empty:
                 return $"~/Themes/Basic/Layouts/Mdw/Empty.cshtml";
+            case RetroHofLayouts.Application:
+                return $"~/Themes/Basic/Layouts/RetroHof/Application.cshtml";
+            case RetroHofLayouts.Account:
+                return $"~/Themes/Basic/Layouts/RetroHof/Account.cshtml";
+            case RetroHofLayouts.Empty:
+                return $"~/Themes/Basic/Layouts/RetroHof/Empty.cshtml";
+            case South25Layouts.Application:
+                return $"~/Themes/Basic/Layouts/South25/Application.cshtml";
+            case South25Layouts.Account:
+                return $"~/Themes/Basic/Layouts/South25/Account.cshtml";
+            case South25Layouts.Empty:
+                return $"~/Themes/Basic/Layouts/South25/Empty.cshtml";
             default:
                 throw new FileNotFoundException(nameof(name));
                 //return fallbackToDefault ? "~/Themes/Basic/Layouts/Application.cshtml" : null;
@@ -38,7 +50,7 @@ public class BasicTheme : ITheme, ITransientDependency
     }
 }
 
-public static class ErindOnTrackLayouts
+public static class EoTLayouts
 {
     public const string Application = "ErindOnTrack.Application";
 
@@ -50,19 +62,29 @@ public static class ErindOnTrackLayouts
 
     public const string Empty = "ErindOnTrack.Empty";
 }
-public static class CanvasLayouts
+public static class South25Layouts
 {
-    public const string Application = "Canvas.Application";
+    public const string Application = "South25.Application";
 
-    public const string Admin = "Canvas.Admin";
+    public const string Admin = "South25.Admin";
 
-    public const string Public = "Canvas.Public";
+    public const string Public = "South25.Public";
 
-    public const string Account = "Canvas.Account";
+    public const string Account = "South25.Account";
 
-    public const string Empty = "Canvas.Empty";
+    public const string Empty = "South25.Empty";
 }
 
+public static class RetroHofLayouts
+{
+    public const string Application = "RetroHof.Application";
+
+    public const string Account = "RetroHof.Account";
+
+    public const string Public = "RetroHof.Public";
+
+    public const string Empty = "RetroHof.Empty";
+}
 public static class MdwLayouts
 {
     public const string Application = "Mdw.Application";
--- src/Retrohof.Web/RetrohofBrandingProvider.cs
+++ src/Retrohof.Web/RetrohofBrandingProvider.cs
@@ -7,8 +7,7 @@ namespace Retrohof.Web;
 [Dependency(ReplaceServices = true)]
 public class RetrohofBrandingProvider : AgileCmsBrandingProvider
 {
-    public override string AppName => _currentTenant?.Name?.ToLower() ?? "Agile SEO Admin";
-    public override string? ShortName => _currentTenant?.Name ?? "Default";
+    public override string AppName => _currentTenant?.Name?.ToLower() ?? "Default";
 
     public RetrohofBrandingProvider(ICurrentTenant currentTenant) : base(currentTenant)
     {
