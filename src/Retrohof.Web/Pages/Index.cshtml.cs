76e51522976f52e07e536b873d8287070c86b192
agile-seo-dev <agile-seo-dev@users.noreply.github.com>
2024-03-15 23:36:08 +0000

    Displays info (badly)

--- src/Retrohof.Web/Pages/Index.cshtml.cs
+++ src/Retrohof.Web/Pages/Index.cshtml.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using Volo.Abp.AspNetCore.Mvc.UI.Theme.Basic;
+using Volo.Abp.MultiTenancy;
+using Volo.Abp.TenantManagement;
+using Volo.Abp.Users;
 
 namespace Retrohof.Web.Pages;
 
@@ -15,7 +19,17 @@ public class IndexModel : RetrohofPageModel
     [BindProperty(SupportsGet = true)]
     public string Login => L["Login"];
 
-    public void OnGet()
+	public string DisplayUser(ICurrentUser user)
+	{
+		return $"User ID:{Environment.NewLine} ID:{user.Id}{Environment.NewLine} {Environment.NewLine}  Name:{user.Name}";
+	}
+
+	public string DisplayTenant(ICurrentTenant tenant)
+	{
+		return $"Tenant ID:{Environment.NewLine} ID:{tenant.Id}{Environment.NewLine} {Environment.NewLine}  Name:{tenant.Name}";
+	}
+
+	public void OnGet()
     {
    }
 }
